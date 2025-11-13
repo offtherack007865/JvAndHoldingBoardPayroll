3af29b5772b4cb83a39f1513dc2af1010b04b526
Morrison <pwmorrison@summithealthcare.com>
2025-11-13 14:18:07 -0500

    Start Web API

---
+++ JvAndHoldingBoardPayroll9/JvAndHoldingBoardPayroll9.Data/Models/CommitteePayrollContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace JvAndHoldingBoardPayroll9.Data.Models;
+
+public partial class CommitteePayrollContext : DbContext
+{
+    public CommitteePayrollContext(DbContextOptions<CommitteePayrollContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<BoardMemberAssignment> BoardMemberAssignments { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<BoardMemberAssignment>(entity =>
+        {
+            entity.HasKey(e => e.BoardMemberAssignmentId).HasName("pk_payroll_jvholdBoardMemberAssignment");
+
+            entity.ToTable("BoardMemberAssignment", "payroll_jvhold");
+
+            entity.Property(e => e.BoardMemberAssignmentId).HasColumnName("BoardMemberAssignmentID");
+            entity.Property(e => e.BoardMemberContactInfoId).HasColumnName("BoardMemberContactInfoID");
+            entity.Property(e => e.PayRateForBoardPositionId).HasColumnName("PayRateForBoardPositionID");
+            entity.Property(e => e.TenureEndDate).HasColumnType("datetime");
+            entity.Property(e => e.TenureStartDate).HasColumnType("datetime");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ JvAndHoldingBoardPayroll9/JvAndHoldingBoardPayroll9.Data/MyConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace JvAndHoldingBoardPayroll9.Data
+{
+    public class MyConstants
+    {
+        public const string AppSettingsFile = "appsettings.json";
+        public const string ConnectionString = "ConnectionString";
+        public const string MyAppName = "EmailWebApiLand9";
+
+        public string BaseWebUrl { get; set; }
+    }
+}
